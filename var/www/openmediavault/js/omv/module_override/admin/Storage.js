f317d0fdf2d9f1a333cba3d1080310c81acb9e4a
Simon Pascal Baur <sbausis@gmx.net>
2015-12-19 22:10:26 +0100

    added override for /storage/mdadm (new Name SoftwareRaid)

--- var/www/openmediavault/js/omv/module_override/admin/Storage.js
+++ var/www/openmediavault/js/omv/module_override/admin/Storage.js
@@ -6,3 +6,11 @@ OMV.WorkspaceManager.registerNodeOverride({
 	path: "/",
 	position: 48
 });
+
+OMV.WorkspaceManager.registerPanelOverride({
+	id: "devices",
+	path: "/storage/mdadm",
+	text: _("SoftwareRaid"),
+	position: 1,
+	//className: "OMV.module.admin.storage.mdadm.Devices"
+});
